a2921656a522f6800bcd41180816b6b7713607d8
eugene <eugene@umputn.com>
2021-07-18 15:11:55 -0500

    add prorgress echo

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,15 @@
 
 echo "Deploying..."
 sudo rm -rf /tmp/remark42-site.git
+echo "Cloning..."
 cd /tmp && git clone git@github.com:umputun/remark-site.git --recurse-submodules remark42-site.git
 cd /tmp/remark42-site.git 
 docker rm -f remark42-site
+echo "Building..."
 docker-compose build && docker-compose up && docker-compose rm -f
+echo "Copying..."
 rm -rf /srv/www/remark42.com/
 mkdir -p /srv/www/remark42.com
 cp -rf /tmp/remark42-site.git/public /srv/www/remark42.com 
 sudo rm -rf /tmp/remark42-site.git
+echo "Done"
